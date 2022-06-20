6b884e406c90ec7e337fd4a7185976c6d9f384d4
Djibril Bathily <bathilydbl@gmail.com>
2022-06-20 22:29:16 +0200

    product card design

--- src/components/Productlist.js
+++ src/components/Productlist.js
@@ -1,6 +1,7 @@
 import "../App.scss";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Rating } from "react-simple-star-rating";
 
 function Productlist() {
   const [data, setData] = useState(null);
@@ -27,9 +28,33 @@ function Productlist() {
                 <Link to={`/product/${product.id}`}>
                   <div className="product">
                     <img src={product.images.photos[0]} />
-                    <p className="product_price">{product.title}</p>
+                    <p className="product_title">{product.title}</p>
                     <p className="product_category">{product.category}</p>
-                    <strong>{product.price}</strong>
+                    <div className="product_rating">
+                      <Rating
+                        readonly={true}
+                        size={20}
+                        initialValue={product.rating}
+                      />
+                      <span className="product_raters">
+                        {" "}
+                        ({product.raters ? product.raters : 0})
+                      </span>
+                    </div>
+                    {product.priceDiscount ? (
+                      <div className="prices">
+                        <span className="product_price">
+                          {product.priceDiscount}
+                        </span>
+                        <span className="product_dicountPrice">
+                          {product.price}
+                        </span>
+                      </div>
+                    ) : (
+                      <div className="prices">
+                        <span className="product_price">{product.price}</span>
+                      </div>
+                    )}
                   </div>
                 </Link>
               </div>
